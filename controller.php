1735c69ffe035f479ef6101098cc8cbeaa32e600
Sara Canovic <saracanovic@gmail.com>
2021-09-11 23:32:13 +0200

    Izmena korpe

--- controller.php
+++ controller.php
@@ -69,6 +69,64 @@
 		}
 
 	}
+    if(isset($_GET['akcija']) && $_GET['akcija']=="vratiProizvodeIzKorpe") {
+		if(isset($_SESSION['korpa'])) {
+			$niz = array();
+			$sql="SELECT * FROM products";
+			if($q=$mysqli->query($sql)) {
+				$proizvodi = array();
+				while($red = $q->fetch_object()) {
+					$proizvodi[$red->id] = $red;
+				}
+				foreach ($_SESSION['korpa'] as $key => $value) {
+					$odgovor['proizvod_id'] = $key;
+					$odgovor['proizvod'] = $proizvodi[$key]->product_name;
+					$odgovor['kolicina'] = $value;
+					$odgovor['cena'] = $proizvodi[$key]->price;
+					$odgovor['ukupno'] = floatval($proizvodi[$key]->price)*$value;
+					$niz[] = $odgovor;
+				}
+				echo json_encode($niz);
+			} else {
+				$odgovor['status'] = 0;
+				$odgovor['poruka'] = "Greska sa bazom";
+				echo json_encode($odgovor);
+			}
+		} else {
+			$odgovor['status'] = 0;
+			$odgovor['poruka'] = "Nema proizvoda u korpi.";
+			echo json_encode($odgovor);
+		}
+	}
+    if(isset($_GET['akcija']) && $_GET['akcija']=="posaljiPorudzbinu") {
+		$podaci_json = file_get_contents("php://input");
+		$podaci = json_decode($podaci_json);
+		$adresa = $mysqli->real_escape_string($podaci->adresa);
+		$sql="INSERT INTO orders (user_id, time_ordered, address) VALUES ('".$_SESSION['userid']."', NOW(), '".$adresa."')";
+		if($q=$mysqli->query($sql)){
+			$order_id = $mysqli->insert_id;
+			foreach ($_SESSION['korpa'] as $key => $value) {
+					$sql2="INSERT INTO orders_list (order_id, product_id, quantity) VALUES('".$order_id."', '".$key."', '".$value."')";
+					$q2=$mysqli->query($sql2);
+				}
+			unset($_SESSION['korpa']);
+			$odgovor['status'] = 1;
+			$odgovor['poruka'] = "Porudzbina je poslata!";
+			echo json_encode($odgovor);
+		} else {
+			$odgovor['status'] = 0;
+			$odgovor['poruka'] = "Greska sa bazom".mysqli_error($mysqli);
+			echo json_encode($odgovor);
+		}
+
+	}
+    if(isset($_GET['akcija']) && $_GET['akcija']=="obrisiProizvodIzKorpe") {
+		$id_proizvoda = $_GET['proizvod_id'];
+		unset($_SESSION['korpa'][$id_proizvoda]);
+		$odgovor['poruka'] = "Proizvod je uspesno obrisan iz korpe.";
+		echo json_encode($odgovor);
+
+	}
 
 	
 	
---
+++ korpa.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+if(!isset($_SESSION['userid'])) {
+  header("Location: login.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SportPro</title>
+	
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Oswald:400,700&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body onload="ucitajKorpu();">
+  <?php include "inc/nav.php"; ?>
+  <div class="container">
+    
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="page-header">
+          <h1>Korpa</h1>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <button class="btn btn-success btn-lg" onclick="otvoriModal();">Naruci</button>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Naziv proizvod</th>
+              <th>Cena</th>
+              <th>Kolicina</th>
+              <th>Ukupno</th>
+              <th>Obrisi</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="myModalLabel">Unos adrese</h4>
+        </div>
+        <div class="modal-body">
+          <p> Unesite adresu:</p>
+          <div class="form-group">
+            <label for="adresa">Adresa</label>
+            <input type="text" class="form-control" id="adresa" name="adresa" placeholder="Adresa...">
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Zatvori</button>
+          <button type="button" class="btn btn-primary" onclick="posaljiPorudzbinu();">Posalji</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+ 
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  <script>
+    function ucitajKorpu() {
+      $.get( "controller.php?akcija=vratiProizvodeIzKorpe", function( json ) {
+        var data = JSON.parse(json);
+        $("table tbody").empty();
+        if(data.status==0) {
+          alert(data.poruka);
+        } else {
+          $.each( data, function( key, value ) {
+            $("table tbody").append("<tr><td>"+value.proizvod+"</td><td>"+value.cena+" EUR</td><<td>"+value.kolicina+"</td>td><td>"+value.ukupno+" EUR</td><td><button class='btn btn-primary btn-sm' onclick='obrisiIzKorpe("+value.proizvod_id+")'>Obrisi proizvod</button></td></tr>");
+          });
+        }
+      });
+    }
+
+    function posaljiPorudzbinu() {
+      var adresa = $("#adresa").val();
+      if(adresa.length == 0) {
+        alert("Unesite adresu!"); 
+      } else {
+        var json_proizvod = '{"adresa":"'+adresa+'"}';
+        console.log(json_proizvod);
+        $.post( "controller.php?akcija=posaljiPorudzbinu", json_proizvod , function( json ) {
+          var data = JSON.parse(json);
+          alert(data.poruka);
+           $(".modal").modal("hide");
+           $("#adresa").val("");
+           ucitajKorpu();
+        });
+      }
+      
+    }
+    
+    function otvoriModal() {
+      $(".modal").modal("show");
+    }
+
+
+    function obrisiIzKorpe(id) {
+      $.get( "controller.php?akcija=obrisiProizvodIzKorpe&proizvod_id="+id, function( json ) {
+        var data = JSON.parse(json);
+        alert(data.poruka);
+        ucitajKorpu();
+      });
+    }
+
+  </script>
+</body>
+</html>
